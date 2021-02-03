d79e2847ac1a36cc0367e6090ee5eab0277954bb
Max Wilson <max@lettingweb.com>
2021-02-03 22:01:49 +0000

    Started on project panel, removed unneeded CSS

--- src/components/ProjectPanel.svelte
+++ src/components/ProjectPanel.svelte
@@ -1,10 +1,23 @@
+<script>
+	import { fly } from "svelte/transition";
+	import { quadOut, sineOut } from 'svelte/easing';
+
+	let showPanel = false;
+
+	function togglePanel() {
+		showPanel = !showPanel;
+	}
+</script>
+
 <div id="project-panel" class="pos-fixed center-full">
-	<div id="panel-open" class="center-full">
+	<div id="panel-open" class="center-full {showPanel ? 'open' : ''}" on:click={togglePanel}>
 		<i class="fas fa-angle-left" />
 	</div>
-	<ul role="list">
-		<li class="project">
-			<span>Quick Tasks</span>
-		</li>
-	</ul>
+	{#if showPanel}
+		<ul id="panel-list" role="list" transition:fly={{ x: 200, opacity: 1, duration: 400, easing: sineOut }}>
+			<li class="project">
+				<span>Quick Tasks</span>
+			</li>
+		</ul>
+	{/if}
 </div>
