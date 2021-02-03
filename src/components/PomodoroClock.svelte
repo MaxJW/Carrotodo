7f0d5b089353321052998e2292c8a0d417857475
Max Wilson <max@lettingweb.com>
2021-02-03 11:28:03 +0000

    Fixed timer with current implementation
    Working on a new implementation with better stores

--- src/components/PomodoroClock.svelte
+++ src/components/PomodoroClock.svelte
@@ -3,52 +3,51 @@
 
     const padWithZeroes = (number) => number.toString().padStart(2, "0");
 
-    let timer = toWait = 25 * 60 // -1 to adjust for extra second
+    let timer = (toWait = 25 * 60); // -1 to adjust for extra second
     let elapsedTime = 0;
-    let start = new Date().getTime();
+    let start = $time.getTime();
     let running = false;
     let started = false;
 
     function pauseTimer() {
-        elapsedTime = Math.floor($time.getTime() - start);
+        elapsedTime = $time.getTime() - start + elapsedTime;
         running = false;
     }
 
     function resumeTimer() {
-        start = new Date().getTime();
+        start = $time.getTime();
         running = true;
     }
 
     function resetTimer() {
         started = running = false;
+        elapsedTime = 0;
         toWait = timer;
     }
 
     function startTimer() {
-        start = new Date().getTime();
+        start = $time.getTime();
         started = running = true;
     }
 
     $: currTime = running
         ? Math.floor(($time.getTime() - start + elapsedTime) / 1000)
         : currTime;
-    $: toWait = running
-        ? timer - currTime > 0
-            ? timer - currTime
-            : 0
-        : toWait;
+    $: toWait = timer - currTime > 0 ? timer - currTime : 0;
     $: minutes = Math.floor(toWait / 60);
-    $: seconds = toWait - minutes * 60;
+    $: seconds = Math.floor(toWait % 60);
 </script>
 
-<div id="pomodoro-clock">{padWithZeroes(minutes)}:{padWithZeroes(seconds)}</div>
-<div id="start-stop">
-    {#if !started}
-        <button type="button" on:click={startTimer}>Start</button>
-    {:else}
-        <button type="button" on:click={running ? pauseTimer : resumeTimer}
-            >{running ? "Pause" : "Resume"}</button
-        >
-        <button type="button" on:click={resetTimer}>Reset</button>
-    {/if}
+<div id="pomodoro-timer" class="center-full">
+    <div id="pomo-clock">{padWithZeroes(minutes)}:{padWithZeroes(seconds)}</div>
+    <div id="pomo-controls">
+        {#if !started}
+            <button type="button" on:click={startTimer}>Start</button>
+        {:else}
+            <button type="button" on:click={running ? pauseTimer : resumeTimer}
+                >{running ? "Pause" : "Resume"}</button
+            >
+            <button type="button" on:click={resetTimer}>Reset</button>
+        {/if}
+    </div>
 </div>
